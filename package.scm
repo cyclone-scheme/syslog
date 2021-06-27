7cd9ae73f5470289099b62b83a61ae4c2931239f
Justin Ethier <justin.ethier@gmail.com>
2021-06-26 18:44:12 -0700

    Use semantic version

--- package.scm
+++ package.scm
@@ -1,6 +1,6 @@
 (package
  (name           syslog)
- (version        0.1)
+ (version        "0.1.1")
  (license        "MIT")
  (authors        "Justin Ethier")
  (maintainers    "Justin Ethier <justin.ethier at gmail dot com>")
